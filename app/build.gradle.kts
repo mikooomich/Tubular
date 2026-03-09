d2a454a23c5f9129aa66b6c3b899e7d96fed205a
Michael Zh <mikooomichael@gmail.com>
2026-03-09 19:29:06 -0400

    DNM: Use java 21

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,7 +21,7 @@ val gitWorkingBranch = providers.exec {
 
 java {
     toolchain {
-        languageVersion = JavaLanguageVersion.of(17)
+        languageVersion = JavaLanguageVersion.of(21)
     }
 }
 
